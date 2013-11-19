34095aee6cfe73097af0c4b30e987fa58897d414
MoonIk <mooneegee@naver.com>
2013-11-20 01:15:32 +0900

    * Modified NNParticle and NNParticleSystem Object

---
+++ NNGameFramework/NNGameFramework/NNParticle.cpp
@@ -0,0 +1,22 @@
+
+#include "NNParticle.h"
+
+NNParticle::NNParticle()
+{
+
+}
+
+NNParticle::~NNParticle()
+{
+
+}
+
+void NNParticle::Render()
+{
+
+}
+
+void NNParticle::Update( float dTime )
+{
+
+}
---
+++ NNGameFramework/NNGameFramework/NNParticleSystem.cpp
@@ -0,0 +1,22 @@
+
+#include "NNParticleSystem.h"
+
+NNParticleSystem::NNParticleSystem()
+{
+
+}
+
+NNParticleSystem::~NNParticleSystem()
+{
+
+}
+
+void NNParticleSystem::Render()
+{
+
+}
+
+void NNParticleSystem::Update( float dTime )
+{
+
+}
