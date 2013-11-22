950ec3bc9ecc1c53972a2e71d02840b5c07fb5cd
kciter <kciter@naver.com>
2013-11-23 04:32:19 +0900

    * Added annotation in NNColor

--- NNGameFramework/NNGameFramework/NNColor.h
+++ NNGameFramework/NNGameFramework/NNColor.h
@@ -1,6 +1,13 @@
 
 #pragma once
 
+/* */
+/* NNColor
+/* 컬러 정보를 담고있는 클래스
+/* RGB값과 투명도 정보를 담고있다.
+/* 투명도를 Alpha값으로 변환할 수 있다.
+/* */
+
 class NNColor
 {
 public:
@@ -12,9 +19,9 @@ public:
 		: m_Red(r), m_Green(g), m_Blue(b), m_Opacity(opacity) {}
 	NNColor( NNColor& color )
 		: m_Red(color.GetRed()), m_Green(color.GetGreen()), m_Blue(color.GetBlue()), m_Opacity(color.GetOpacity()) {}
-	~NNColor()
-	{
-	}
+	/* 생성자 */
+	~NNColor() {}
+	/* 소멸자 */
 
 	void SetRGB( int r, int g, int b )
 	{
@@ -22,6 +29,7 @@ public:
 		m_Green = g;
 		m_Blue = b;
 	}
+	/* RGB값 설정 */
 	void SetRGBA( int r, int g, int b, int a )
 	{
 		m_Red = r;
@@ -29,17 +37,28 @@ public:
 		m_Blue = b;
 		m_Opacity = static_cast<float>(a/255.f);
 	}
+	/* RGB, Alpha값 설정 */
 	void SetRed( int r ) { m_Red = r; }
+	/* RGB값 중 R의 값만 설정 */
 	void SetGreen( int g ) { m_Green = g; }
+	/* RGB값 중 G의 값만 설정 */
 	void SetBlue( int b ) { m_Blue = b; }
+	/* RGB값 중 B의 값만 설정 */
 	void SetOpacity( float opacity ) { m_Opacity = opacity; }
+	/* 투명도 값 설정 */
 	void SetAlpha( int a ) { m_Opacity = static_cast<float>(a/255.f); }
+	/* Alpha값 설정 */
 
 	int GetRed() const { return m_Red; }
+	/* RGB값 중 R의 값 반환 */
 	int GetGreen() const { return m_Green; }
+	/* RGB값 중 G의 값 반환 */
 	int GetBlue() const { return m_Blue; }
+	/* RGB값 중 B의 값 반환 */
 	float GetOpacity() const { return m_Opacity; }
+	/* 투명도 값 반환 */
 	int GetAlpha() const { return static_cast<int>(m_Opacity*255); }
+	/* Alpha값 반환 */
 
 public:
 	NNColor& operator= ( const NNColor& color );
@@ -48,6 +67,7 @@ public:
 	NNColor operator-() const;
 	NNColor operator*( float n ) const;
 	NNColor operator/( float n ) const;
+	/* 연산자 오버로딩 */
 
 private:
 	int m_Red;
