d877b232d07a3b313c2694f21ff08304057ecb4b
mooneegee <mooneegee@naver.com>
2013-11-26 03:56:21 +0900

    NNParticleSystem.h get method

--- NNGameFramework/NNGameFramework/NNParticleSystem.h
+++ NNGameFramework/NNGameFramework/NNParticleSystem.h
@@ -17,8 +17,48 @@ public:
 	static NNParticleSystem* Create();
 
 public:
-	bool GetMinLifeTime() const { return m_MinLifeTime; }
+	float GetMinLifeTime() const { return m_MinLifeTime; }
+	float GetMaxLifeTime() const { return m_MaxLifeTime; }
 
+	float GetMinStartSpeed() const { return m_MinStartSpeed; }
+	float GetMaxStartSpeed() const { return m_MaxStartSpeed; }
+
+	float GetMinEndSpeed() const { return m_MinEndSpeed; }
+	float GetMaxEndSpeed() const { return m_MaxEndSpeed; }
+
+	float GetMinStartRotationSpeed() const { return m_MinStartRotationSpeed; }
+	float GetMaxStartRotationSpeed() const { return m_MaxStartRotationSpeed; }
+
+	float GetMinEndRotationSpeed() const { return m_MinEndRotationSpeed; }
+	float GetMaxEndRotationSpeed() const { return m_MaxEndRotationSpeed; }
+
+	float GetMinStartScaleX() const { return m_MinStartScaleX; }
+	float GetMaxStartScaleX() const { return m_MaxStartScaleX; }
+
+	float GetMinStartScaleY() const { return m_MinStartScaleY; }
+	float GetMaxStartScaleY() const { return m_MaxStartScaleY; }
+
+	float GetMinEndSacleX() const { return m_MinEndScaleX; }
+	float GetMaxEndSacleX() const { return m_MaxEndScaleX; }
+
+	float GetMinEndSacleY() const { return m_MinEndScaleY; }
+	float GetMaxEndSacleY() const { return m_MaxEndScaleY; }
+
+	float GetStartOpacity() const { return m_StartOpacity; }
+	float GetEndOpacity() const { return m_EndOpacity; }
+
+	float GetDirection() const { return m_Direction; }
+
+	float GetSpreadDegree() const { return m_SpreadDegree; }
+
+	float GetMinStartRodiusX() const { return m_MinStartRadiusX; }
+	float GetMaxStartRodiusX() const { return m_MaxStartRadiusX; }
+
+	float GetMinStartRodiusY() const { return m_MinStartRadiusY; }
+	float GetMaxStartRodiusY() const { return m_MaxStartRadiusY; }
+
+	NNColor GetStartColor() { return m_StartColor; }
+	NNColor GetEndColor() { return m_EndColor; }
 
 private:
 	void CreateParticle();
