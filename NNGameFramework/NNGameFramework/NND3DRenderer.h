2a9e26a17b6b627147786bbd286fd7836d6246b9
kciter <kciter@naver.com>
2013-11-27 20:47:08 +0900

    * D3D Include Header Move To NNIncludeSet

--- NNGameFramework/NNGameFramework/NND3DRenderer.h
+++ NNGameFramework/NNGameFramework/NND3DRenderer.h
@@ -2,8 +2,6 @@
 #pragma once
 
 #include "NNRenderer.h"
-#include "Library/DirectX/Include/d3d9.h"
-#include "Library/DirectX/Include/d3dx9.h"
 
 class NND3DRenderer : public NNRenderer
 {
@@ -18,6 +16,10 @@ public:
 	bool Clear();
 	bool Begin();
 	bool End();
+
+public:
+	LPDIRECT3D9 GetD3D() { return mD3D; }
+	LPDIRECT3DDEVICE9 GetDevice() { return mD3DDevice; }
 
 private:
 	LPDIRECT3D9 mD3D;
