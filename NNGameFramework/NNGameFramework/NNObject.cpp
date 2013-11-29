3a7dc41e337118937add28854ff5aaea6bc6cd48
kciter <kciter@naver.com>
2013-11-29 16:19:05 +0900

    * Fixed Matrix

--- NNGameFramework/NNGameFramework/NNObject.cpp
+++ NNGameFramework/NNGameFramework/NNObject.cpp
@@ -24,8 +24,8 @@ void NNObject::Render()
 	if ( m_Visible == false ) return;
 
 	m_Matrix = D2D1::Matrix3x2F::Translation( -m_Center.GetX(), -m_Center.GetY() )* 
-		D2D1::Matrix3x2F::Rotation( m_Rotation ) *
 		D2D1::Matrix3x2F::Scale( m_ScaleX, m_ScaleY ) *
+		D2D1::Matrix3x2F::Rotation( m_Rotation ) *
 		D2D1::Matrix3x2F::Translation( m_Position.GetX(), m_Position.GetY() );
 
 	if( m_pParent )
