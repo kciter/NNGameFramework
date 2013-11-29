6a79f9d9cbce7f9ce971003952521d24755b3b4a
kciter <kciter@naver.com>
2013-11-29 16:17:24 +0900

    * Modified ZipSample

--- NNGameFramework/Sample/ZipSample.cpp
+++ NNGameFramework/Sample/ZipSample.cpp
@@ -7,25 +7,25 @@
 
 ZipSample::ZipSample()
 {
-	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/Zip/test.zip", L"test.xml");
+	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/map.csm", L"map.xml");
 	m_TestXML = NNResourceManager::GetInstance()->LoadXMLFromMemory( m_loadZip );
-
-	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/Zip/test.zip", L"test.png");
+	/*
+	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/map.csm", L"test.png");
 	m_TestSprite = NNSprite::Create( m_loadZip );
 
-	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/Zip/test.zip", L"test.mp3");
+	m_loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(L"Resources/map.csm", L"test.mp3");
 	m_TestSound = NNResourceManager::GetInstance()->LoadSoundFromMemory( m_loadZip, true );
-
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
-	std::cout << m_TestXML->XPathToString("/MyApp/Messages/Welcome/text()").c_str() << std::endl;
+	*/
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
+	std::cout << m_TestXML->XPathToString("/map/tileInfo/t0-0/@isFull").c_str() << std::endl;
 	std::cout << "xml end" << std::endl;
 
-	AddChild( m_TestSprite );
-	NNAudioSystem::GetInstance()->Play( m_TestSound );
+	//AddChild( m_TestSprite );
+	//NNAudioSystem::GetInstance()->Play( m_TestSound );
 }
 ZipSample::~ZipSample()
 {
