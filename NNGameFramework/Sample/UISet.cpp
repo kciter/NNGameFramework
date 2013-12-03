261cfd213329ebd2b79336f314751ec32766e9eb
kciter <kciter@naver.com>
2013-12-04 02:41:18 +0900

    * Added Init Function

--- NNGameFramework/Sample/UISet.cpp
+++ NNGameFramework/Sample/UISet.cpp
@@ -3,14 +3,17 @@
 
 UISet::UISet()
 {
+}
+UISet::~UISet()
+{
+}
+
+void UISet::Init()
+{
 	m_UI = NNSprite::Create(L"Resources/Texture/walk_0.png");
 	AddChild( m_UI );
 
 	m_UI->SetPosition( 20.f, 20.f );
-}
-UISet::~UISet()
-{
-
 }
 
 void UISet::Render()
