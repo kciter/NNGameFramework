b751d405330a94a3566b0acdecbc4b2882c9ddc7
kciter <kciter@naver.com>
2013-12-05 05:12:39 +0900

    Added D3DTexture

--- NNGameFramework/NNGameFramework/NNTexture.cpp
+++ NNGameFramework/NNGameFramework/NNTexture.cpp
@@ -4,7 +4,7 @@
  * 작성자: 이선협
  * 작성일: 2013. 10. 30
  * 마지막으로 수정한 사람: 이선협
- * 수정일: 2013. 12. 04
+ * 수정일: 2013. 12. 05
  */
 
 #include "NNTexture.h"
@@ -24,6 +24,9 @@ NNTexture* NNTexture::Create( std::wstring path )
 	{
 	case D2D:
 		pInstance = new NND2DTexture( path );
+		break;
+	case D3D:
+		pInstance = new NND3DTexture( path );
 		break;
 	default:
 		break;
@@ -147,11 +150,16 @@ NND3DTexture::NND3DTexture()
 {
 }
 NND3DTexture::NND3DTexture( std::wstring path )
+	: mTexture(nullptr)
 {
-	NND3DRenderer* pD2DRenderer = static_cast<NND3DRenderer*>(NNApplication::GetInstance()->GetRenderer());
+	NND3DRenderer* pD3DRenderer = static_cast<NND3DRenderer*>(NNApplication::GetInstance()->GetRenderer());
 
-	D3DXCreateTextureFromFileEx( pD2DRenderer->GetDevice(), path.c_str(), D3DX_DEFAULT_NONPOW2, D3DX_DEFAULT_NONPOW2,
-		1, 0, D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, NULL, NULL, NULL, &mTexture );
+	m_Path = path;
+
+	LPDIRECT3DDEVICE9 temp = pD3DRenderer->GetDevice();
+
+	HRESULT hr = D3DXCreateTextureFromFileEx( pD3DRenderer->GetDevice(), path.c_str(), D3DX_DEFAULT_NONPOW2, D3DX_DEFAULT_NONPOW2,
+		1, 0, D3DFMT_FROM_FILE, D3DPOOL_DEFAULT, D3DX_DEFAULT, D3DX_DEFAULT, NULL, NULL, NULL, &mTexture );
 }
 NND3DTexture::~NND3DTexture()
 {
