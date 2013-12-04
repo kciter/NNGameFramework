cac3740b96468940f0db00680d513d2494e5ed2e
kciter <kciter@naver.com>
2013-12-05 05:15:57 +0900

    * Added Matrix Function

--- NNGameFramework/NNGameFramework/NNMatrix.cpp
+++ NNGameFramework/NNGameFramework/NNMatrix.cpp
@@ -28,31 +28,27 @@ NNMatrix::~NNMatrix()
 
 void NNMatrix::Identity()
 {
-	_11 = 1.f;
-	_12 = 0.f;
-	_21 = 0.f;
-	_22 = 1.f;
-	_31 = 0.f;
-	_32 = 0.f;
+	_11 = 1.f; _12 = 0.f;
+	_21 = 0.f; _22 = 1.f;
+	_31 = 0.f; _32 = 0.f;
 }
-NNMatrix NNMatrix::Scale( NNPoint size, NNPoint center )
+NNMatrix NNMatrix::Scale( NNPoint size )
 {
 	NNMatrix instance;
-	instance._11 = size.GetX(); instance._12 = 0.0;
-	instance._21 = 0.0; instance._22 = size.GetY();
-	instance._31 = center.GetX() - size.GetX() * center.GetX();
-	instance._32 = center.GetY() - size.GetY() * center.GetY();
+	instance._11 = size.GetX(); instance._12 = 0.0f;
+	instance._21 = 0.0f; instance._22 = size.GetY();
+	instance._31 = 0.0f; instance._32 = 0.0f;
 	return instance;
 }
-NNMatrix NNMatrix::Scale( float x, float y, NNPoint center )
+NNMatrix NNMatrix::Scale( float x, float y )
 {
-	return Scale( NNPoint(x, y), center );
+	return Scale( NNPoint(x, y) );
 }
 NNMatrix NNMatrix::Translate( NNPoint size )
 {
 	NNMatrix instance;
-	instance._11 = 1.0; instance._12 = 0.0;
-	instance._21 = 0.0; instance._22 = 1.0;
+	instance._11 = 1.0f; instance._12 = 0.0f;
+	instance._21 = 0.0f; instance._22 = 1.0f;
 	instance._31 = size.GetX(); instance._32 = size.GetY();
 	return instance;
 }
@@ -60,14 +56,76 @@ NNMatrix NNMatrix::Translate( float x, float y )
 {
 	return Translate( NNPoint(x,y) );
 }
-NNMatrix NNMatrix::Rotation( float angle, NNPoint center )
+NNMatrix NNMatrix::Rotation( float angle )
 {
-	return NNMatrix();
+	NNMatrix instance;
+	instance._11 = cos(NNDegreeToRadian(angle)); instance._12 = -sin(NNDegreeToRadian(angle));
+	instance._21 = sin(NNDegreeToRadian(angle)); instance._22 = cos(NNDegreeToRadian(angle));
+	instance._31 = 0.0f; instance._32 = 0.0f;
+	return instance;
 }
 
-bool NNMatrix::IsIdentity()
+bool NNMatrix::IsIdentity() const
 {
 	return _11 == 1.f && _12 == 0.f
 			&& _21 == 0.f && _22 == 1.f
 			&& _31 == 0.f && _32 == 0.f;
+}
+
+float NNMatrix::Determinant() const
+{
+	return (_11 * _22) - (_12 * _21);
+}
+
+NNMatrix& NNMatrix::operator= ( const NNMatrix& matrix )
+{
+	this->_11 = matrix._11; this->_12 = matrix._12;
+	this->_21 = matrix._21; this->_22 = matrix._22;
+	this->_31 = matrix._31; this->_32 = matrix._32;
+	return *this;
+}
+NNMatrix NNMatrix::operator+( const NNMatrix& matrix ) const
+{
+	return NNMatrix( 
+		this->_11+matrix._11, this->_12+matrix._12,
+		this->_21+matrix._21, this->_22+matrix._22,
+		this->_31+matrix._31, this->_32+matrix._32 );
+}
+NNMatrix NNMatrix::operator-( const NNMatrix& matrix ) const
+{
+	return NNMatrix(
+		this->_11-matrix._11, this->_12-matrix._12,
+		this->_21-matrix._21, this->_22-matrix._22,
+		this->_31-matrix._31, this->_32-matrix._32 );
+}
+NNMatrix NNMatrix::operator-() const
+{
+	return NNMatrix(
+		-this->_11, -this->_12,
+		-this->_21, -this->_22,
+		-this->_31, -this->_32 );
+}
+NNMatrix NNMatrix::operator*( float n ) const
+{
+	return NNMatrix(
+		-this->_11*n, -this->_12*n,
+		-this->_21*n, -this->_22*n,
+		-this->_31*n, -this->_32*n );
+}
+NNMatrix NNMatrix::operator/( float n ) const
+{
+	return NNMatrix(
+		-this->_11/n, -this->_12/n,
+		-this->_21/n, -this->_22/n,
+		-this->_31/n, -this->_32/n );
+}
+NNMatrix NNMatrix::operator*( const NNMatrix& matrix ) const
+{
+	return NNMatrix(
+		this->_11*matrix._11 + this->_12*matrix._21,
+		this->_11*matrix._12 + this->_12*matrix._22,
+		this->_21*matrix._11 + this->_22*matrix._21,
+		this->_21*matrix._12 + this->_22*matrix._22,
+		this->_31*matrix._11 + this->_32*matrix._21 + matrix._31,
+		this->_31*matrix._12 + this->_32*matrix._22 + matrix._32 );
 }
