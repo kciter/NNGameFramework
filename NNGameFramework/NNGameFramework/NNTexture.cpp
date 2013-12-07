77e561f4acee0bcbb8d134fb7a97ca7956c7c040
kciter <kciter@naver.com>
2013-12-07 14:06:16 +0900

    * Create Stream in D3DTexture

--- NNGameFramework/NNGameFramework/NNTexture.cpp
+++ NNGameFramework/NNGameFramework/NNTexture.cpp
@@ -4,7 +4,7 @@
  * 작성자: 이선협
  * 작성일: 2013. 10. 30
  * 마지막으로 수정한 사람: 이선협
- * 수정일: 2013. 12. 05
+ * 수정일: 2013. 12. 07
  */
 
 #include "NNTexture.h"
@@ -158,6 +158,14 @@ NND3DTexture::NND3DTexture( std::wstring path )
 
 	D3DXCreateTextureFromFileEx( pD3DRenderer->GetDevice(), path.c_str(), D3DX_DEFAULT_NONPOW2, D3DX_DEFAULT_NONPOW2,
 		1, 0, D3DFMT_FROM_FILE, D3DPOOL_DEFAULT, D3DX_DEFAULT, D3DX_DEFAULT, NULL, NULL, NULL, &mTexture );
+}
+NND3DTexture::NND3DTexture( char* buf, int size )
+	: mTexture(nullptr)
+{
+	NND3DRenderer* pD3DRenderer = static_cast<NND3DRenderer*>(NNApplication::GetInstance()->GetRenderer());
+
+	D3DXCreateTextureFromFileInMemoryEx( pD3DRenderer->GetDevice(), buf, size, D3DX_DEFAULT_NONPOW2, D3DX_DEFAULT_NONPOW2,
+		1, 0, D3DFMT_FROM_FILE, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, NULL, NULL, NULL, &mTexture );
 }
 NND3DTexture::~NND3DTexture()
 {
--- NNGameFramework/NNGameFramework/NNTexture.h
+++ NNGameFramework/NNGameFramework/NNTexture.h
@@ -4,7 +4,7 @@
  * 작성자: 이선협
  * 작성일: 2013. 10. 30
  * 마지막으로 수정한 사람: 이선협
- * 수정일: 2013. 12. 04
+ * 수정일: 2013. 12. 07
  */
 
 #pragma once
@@ -37,12 +37,10 @@ class NND2DTexture : public NNTexture
 {
 public:
 	NND2DTexture();
-	NND2DTexture( char *buf , int size );
+	NND2DTexture( char *buf, int size );
 	NND2DTexture( std::wstring path );
 	virtual ~NND2DTexture();
 
-	//void Create();
-	//static NND2DTexture* Create( std::wstring path );
 	void Destroy();
 
 public:
@@ -58,6 +56,7 @@ class NND3DTexture : public NNTexture
 {
 public:
 	NND3DTexture();
+	NND3DTexture( char* buf, int size );
 	NND3DTexture( std::wstring path );
 	virtual ~NND3DTexture();
 
